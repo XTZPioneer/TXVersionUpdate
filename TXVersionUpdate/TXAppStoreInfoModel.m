a8b1375d21e6d235bca37cb3d62cf17bb2ccafad
ZhangXiong <xtz_pioneer@163.com>
2017-04-26 13:57:29 +0800

    更新结构

---
+++ TXVersionUpdate/TXAppStoreInfoModel.m
@@ -0,0 +1,14 @@
+//
+//  TXAppStoreInfoModel.m
+//  TXVersionUpdate
+//
+//  Created by  杭州信配iOS开发 on 2017/4/26.
+//  Copyright © 2017年 张天雄. All rights reserved.
+//
+#import "TXAppStoreInfoModel.h"
+
+@implementation TXAppStoreInfoModel : NSObject 
+
+- (void)setValue:(id)value forUndefinedKey:(NSString *)key{}
+
+@end
